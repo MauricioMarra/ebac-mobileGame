b2567495fc809d499302595c58a65e8856f4eee3
MauricioMarra <52969819+MauricioMarra@users.noreply.github.com>
2023-04-16 14:25:41 -0300

    feat: adds sound to player death, particles and animations to menus

--- Assets/Scripts/Helpers/ButtonHelper.cs
+++ Assets/Scripts/Helpers/ButtonHelper.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 
-public class ButtonHelper : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ButtonHelper : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClip;
     private ScreenBase _parentScreen;
     private Vector3 _originalScale;
     private Tween _currentTween;
     private float _scaleUpFactor = 1.3f;
     private float _duration = 0.4f;
-    private bool _isButtonClicked = false;
+
+    private void OnValidate()
+    {
+        if (GetComponent<AudioSource>() == null)
+            _audioSource = gameObject.AddComponent<AudioSource>();
+    }
 
     private void Awake()
     {
@@ -19,11 +26,6 @@ public class ButtonHelper : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     private void Start()
     {
         _parentScreen = GetComponentInParent<ScreenBase>();
-    }
-
-    private void OnDisable()
-    {
-        _isButtonClicked = false;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -44,5 +46,14 @@ public class ButtonHelper : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
     public void ScaleUpButton(float delay = 0)
     {
         this.transform.DOScale(_originalScale, _duration).SetDelay(delay);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //VFXManager.instance.PlayVfx(VfxType.Click, Input.mousePosition);
+        VFXManager.instance.PlayVfx(VfxType.Click, this.transform.position);
+
+        if (_audioClip != null && _audioSource != null)
+            AudioManager.instance.PlaySingle(_audioClip, _audioSource);
     }
 }
